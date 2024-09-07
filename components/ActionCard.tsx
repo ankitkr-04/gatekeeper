3259b2df45a483191e557c6c47d6578124ac0da9
ankitkr-04 <ak0182274@gmail.com>
2024-09-07 11:42:26 +0530

    fix some layout issue in action card

--- components/ActionCard.tsx
+++ components/ActionCard.tsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import { Card, CardContent } from './ui/card'
-import Image from 'next/image'
+import Image from "next/image";
+import Link from "next/link";
+import { Card, CardContent } from "./ui/card";
 
-
-
-const ActionCard = ({value, label, icon, description}: ActionCardProps) => {
+const ActionCard = ({ value, label, icon, description }: ActionCardProps) => {
   return (
-   <Card className='w-64 h-48 rounded-'>
-    <CardContent>
-        <div className='font-inter items-start p-5  '>
-           {icon && <Image src={icon} alt={label} width={50} height={50} />}
-            <h2 className='h3-bold mt-4'>{label}</h2>
-            <p className='text-gray-600'>{description}</p>
+    <Card className="sm:w-64 sm:h-48 rounded-">
+      <CardContent>
+        <Link href={`/${value}`}>
+          <div className="font-inter items-start p-5  ">
+            {icon && <Image src={icon} alt={label} width={50} height={50} />}
+            <h2 className="h3-bold mt-4">{label}</h2>
+            <p className="text-gray-600">{description}</p>
+          </div>
+        </Link>
+      </CardContent>
+    </Card>
+  );
+};
 
-        </div>
-    </CardContent>
-
-   </Card>
-  )
-}
-
-export default ActionCard
+export default ActionCard;
